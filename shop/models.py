3c7bc7d37da442d45fe0bcc926c5181d1c51ad36
subhranil-179 <>
2022-05-08 19:40:48 +0530

    one gif only ;-;, they literally changes their design everyday

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    ID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
